f1443a7238579aa7ca866a4c5a4d0d046c78ac80
Krishna <kpanchap@tenstorrent.com>
2024-06-04 19:05:23 +0000

    updates to dockerfile and run command

---
+++ docker-test.sh
@@ -0,0 +1,13 @@
+cd /home/ubuntu/project-falcon/api-services
+docker run -rm -it \
+    --device /dev/tenstorrent/0:/dev/tenstorrent/0 \
+    -v /dev/hugepages-1G:/dev/hugepages-1G \
+    --shm-size=4g \
+    --cap-add ALL \
+    -v ./inference-api/tt-metal-falcon-7b/src:/mnt/src \
+    -v /home/ubuntu/project-falcon/local_cache_root/:/mnt/local_cache_root/ \
+    -e JWT_SECRET= $(JWT_SECRET) \
+    -e FLASK_SECRET=$(FLASK_SECRET) \
+    -e CACHE_ROOT=/mnt/local_cache_root \
+    -e SERVICE_PORT=7000 \
+    mistral-image bash
